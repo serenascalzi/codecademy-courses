e80de6414a2ceb18a12b4ec7d32850f2c1c0c445
serenascalzi <serenascalzi@alumni.iu.edu>
2020-01-21 14:28:13 -0800

    add java lesson 5/13

--- java-course/code.java
+++ java-course/code.java
@@ -56,7 +56,50 @@
 		}
 	}
 
-// Methods
+// Methods - SavingsAccount.java
+	public class SavingsAccount {
+		int balance;
+
+		public SavingsAccount(int initialBalance){
+			balance = initialBalance;
+		}
+
+		public void checkBalance() {
+			System.out.println("Hello!");
+			System.out.println("Your balance is " + balance);
+		}
+
+		public void deposit(int amountToDeposit) {
+			int afterDeposit = balance + amountToDeposit;
+			balance = afterDeposit;
+			System.out.println("You just deposited " + amountToDeposit);
+		}
+
+		public int withdraw(int amountToWithdraw) {
+			int afterWithdraw = balance - amountToWithdraw;
+			balance = afterWithdraw;
+			System.out.println("You just withdrew " + amountToWithdraw);
+			return amountToWithdraw;
+		}
+
+		public static void main(String[] args){
+			SavingsAccount savings = new SavingsAccount(2000);
+
+			savings.checkBalance();	// Check Balance
+
+			savings.withdraw(300);	// Withdraw 300
+
+			savings.checkBalance();	// Check Balance
+
+			savings.deposit(600);	// Deposit 600
+
+			savings.checkBalance();	// Check Balance
+
+			savings.deposit(600);	// Deposit 600
+
+			savings.checkBalance();	// Check Balance
+		}
+	}
 
 // Conditionals and Control Flow
 
